48f844d6a9bb95561e617747916ddb021b1e24a1
Paige Hensley <paige@papermountain.org>
2022-09-10 19:12:17 -0400

    Clean up

--- openml_scraper.py
+++ openml_scraper.py
@@ -19,11 +19,6 @@ oml.config.apikey = 'YOUR API KEY HERE'
 
 def generate_tables(input_id):
     """Gather run info."""
-    current_scores = []
-    current_runs = []
-    current_flows = []
-    j = 1
-    k = 1
     run_keys = []
     run_values = []
 
@@ -31,14 +26,15 @@ def generate_tables(input_id):
     run_evals = run.evaluations
     flow_params = run.parameter_settings
 
-    current_runs.append(
+    current_runs = [
         {"run info": run, "run id": run.run_id, "flow name": run.flow_name,
          "flow id": run.flow_id, "task type": run.task_type, "task id": run.task_id,
-         "setup id": run.setup_id, "dataset_id": run.dataset_id})
+         "setup id": run.setup_id, "dataset_id": run.dataset_id}]
 
-    for measure, value in run_evals.items():
-        current_scores.append({measure: value})
+    current_scores = [{measure: value} for measure, value in run_evals.items()]
 
+    j = 1
+    k = 1
     for dic in flow_params:
         for key in dic:
             if j % 3 != 0:
@@ -52,9 +48,7 @@ def generate_tables(input_id):
                 else:
                     j += 1
 
-    flow_dict = dict(zip(run_keys, run_values))
-    for key in flow_dict:
-        current_flows.append({key: flow_dict.get(key)})
+    current_flows = [{key: value} for key, value in zip(run_keys, run_values)]
 
     results = [current_runs, current_flows, current_scores]
     return results
@@ -66,8 +60,8 @@ def gen_file_name(run_num):
     name = '.'.join(name_split)
     sheet_path_split = ['.', name]
     sheet_path = '/'.join(sheet_path_split)
-    results = [name, sheet_path]
-    return results
+
+    return sheet_path
 
 
 def multiple_dfs(df_list, sheets, spaces, file_path):
@@ -84,36 +78,28 @@ def gen_run_table(run_info):
     """Generate dataframe of run info."""
     run_table = pd.DataFrame(run_info)
     run_dict = run_info[0]
-    run_lables = []
-    for pair in run_dict:
-        run_lables.append(pair)
 
-    run_table.columns = run_lables
+    run_labels = [pair for pair in run_dict]
+
+    run_table.columns = run_labels
 
     return run_table
 
 
 def gen_flow_table(flow_info):
     """Generate table of flow info."""
-    flow_table = pd.DataFrame(flow_info)
-
-    return flow_table
+    return pd.DataFrame(flow_info)
 
 
 def gen_score_table(score_info):
     """Generate table of score info."""
-    score_table = pd.DataFrame(score_info)
-
-    return score_table
+    return pd.DataFrame(score_info)
 
 
 def gen_full_run_file(run_id):
     """Execute all other functions to form excel sheet."""
     try:
-        output = generate_tables(run_id)
-        runs = output[0]
-        flows = output[1]
-        scores = output[2]
+        runs, flows, scores = generate_tables(run_id)
 
         current_run_table = gen_run_table(runs)
         current_flow_table = gen_flow_table(flows)
@@ -122,9 +108,7 @@ def gen_full_run_file(run_id):
         # list of dataframes
         dfs = [current_run_table, current_flow_table, current_score_table]
 
-        file_names = gen_file_name(run_id)
-        # current_name = file_names[0]
-        current_path = file_names[1]
+        current_path = gen_file_name(run_id)
 
         # run sheet generation function
         multiple_dfs(dfs, 'Validation', 1, current_path)
@@ -138,6 +122,7 @@ NUM_RUNS = 1
 
 futures = []
 i = 0
+
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
     while i <= NUM_RUNS:
